6e0619e827153bf32db1c8644c334bf0f69a8655
Nelson <nelsonsprague84@gmail.com>
2019-06-07 17:23:31 -0600

    something with some frustration

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,69 @@
 clockRunning = false;
 var number = 10;
 var countdown;
+
+var resultsContain = document.getElementById('results');
 // timer that starts at the start of the game and ends the game
 function clockstart(){
     clearInterval(countdown);
     clockRunning = true;
-    createQuestions();
+    createTrivia();
     countdown = setInterval(decrement, 1000);
 console.log(countdown)
 }
 
 function decrement(){
-    number--;
     $("#timeRemain").html("<h2>" + number + "</h2>")
     if (number === 0){
         gamestop()
     }
+    number--;
 }
 
 function gamestop(){
     alert("Times UP!");
     clearInterval(countdown);
-    answerReveal();
+    showResults();
 }
 // series of questions with a multipule choice or true false answers where they can only pick one answer 
-function createQuestions(){
-
-    // question list  in random order
-    // coordinating answers in random order  
+function createTrivia(){
+    $("#trivia").empty();
 }
-// click event to record useranswer only one answer
+
+                // var myQuestions = [
+                //     {question: "Who is the strongest?",
+                // answers:{
+                //     a: "Superman",
+                //     b: "The Terminator",
+                //     c: "Waluigi"
+                // },
+                //   correctAnswer: "c"
+                // },
+                // {question: "Who is the strongest?",
+                // answers:{
+                //     a: "Superman",
+                //     b: "The Terminator",
+                //     c: "Waluigi"
+                // },
+                //   correctAnswer: "c"
+                // },
+                // {question: "Who is the strongest?",
+                // answers:{
+                //     a: "Superman",
+                //     b: "The Terminator",
+                //     c: "Waluigi"
+                // },
+                //   correctAnswer: "c"
+                // },
+                // ];
+                //      for(var i = 0; i < myQuestions.length; i++){
+                //          console.log(myQuestions[i])
+                //      }
 // $("#answer").on("click")
 
 
 // at the end of the game reveal the number of questions that players answer correctly and incorrectly 
-function answerReveal(){
+function showResults(){
     //show correct answer vs user answer
 }
 clockstart();
